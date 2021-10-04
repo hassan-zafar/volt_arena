1d6a4d817d533593aeb92f196545eb48925240b1
Hassan Ayoub Zafar <hz.asd1@gmail.com>
2021-10-05 02:46:43 +0500

    added products model

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -6,21 +6,38 @@ class Product with ChangeNotifier {
   final String? description;
   final double? price;
   final String? imageUrl;
-  final String? productCategoryName;
-  final String? brand;
+  final bool? isIndividual;
   final int? quantity;
   final bool? isFavorite;
   final bool? isPopular;
-
-  Product(
-      {this.id,
-      this.title,
-      this.description,
-      this.price,
-      this.imageUrl,
-      this.productCategoryName,
-      this.brand,
-      this.quantity,
-      this.isFavorite,
-      this.isPopular});
+  final int? pallets;
+  final int? groupMembers;
+  Product({
+    this.id,
+    this.title,
+    this.description,
+    this.price,
+    this.imageUrl,
+    this.groupMembers,
+    this.isIndividual,
+    this.quantity,
+    this.isFavorite,
+    this.isPopular,
+    this.pallets,
+  });
+  factory Product.fromDocument(doc) {
+    return Product(
+      id: doc.data()["id"],
+      title: doc.data()["title"],
+      description: doc.data()["description"],
+      price: doc.data()["price"],
+      imageUrl: doc.data()["imageUrl"],
+      groupMembers: doc.data()["groupMembers"],
+      isIndividual: doc.data()["isIndividual"],
+      quantity: doc.data()["quantity"],
+      isFavorite: doc.data()["isFavorite"],
+      isPopular: doc.data()["isPopular"],
+      pallets: doc.data()["pallets"],
+    );
+  }
 }
