05b3d711a391bb1b231d0343ada769f9a388823d
Hassan Ayoub Zafar <hz.asd1@gmail.com>
2021-10-05 10:54:38 +0500

    Update product_details.dart

--- lib/inner_screens/product_details.dart
+++ lib/inner_screens/product_details.dart
@@ -367,7 +367,7 @@ class _ProductDetailsState extends State<ProductDetails> {
                       color: Theme.of(context).backgroundColor,
                       onPressed: () {
                         Navigator.of(context)
-                            .pushNamed(CalenderScreen.routeName)
+                            .push(MaterialPageRoute(builder: (context)=>CalenderScreen(gameTime: int.parse(prodAttr.gameTime!),)))
                             .then((value) => Navigator.pop(context));
                       },
                       child: Row(
